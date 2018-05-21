d582c95bf15c37d812eb9e9e754d2c85138cf195
Phil Rukin <philipp@rukin.me>
2018-05-21 16:48:43 +0200

    Don't check stories that were already posted

--- main.py
+++ main.py
@@ -51,7 +51,15 @@ def task(stories):
         logging.info('STOP: {id} has low score ({score})'.format(**story))
     except urlfetch.DownloadError as ex:
       logging.exception(ex)
-  rpcs = map(lambda id: item_async(id, check_story), stories)
+
+  # stringify ids for use in memcache and convert to set for later
+  ids = set(str(story_id) for story_id in stories)
+  # get stories that we already posted to reduce the number of requests
+  cached_stories = set(memcache.get_multi(ids).keys())
+  logging.info('cached stories: {}'.format(cached_stories))
+  # remove stories we know about from stories that we need to check
+  stories_to_check = ids.difference(cached_stories)
+  rpcs = map(lambda id: item_async(id, check_story), stories_to_check)
   for rpc in rpcs:
     rpc.wait()
 

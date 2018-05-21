52d7568713be5f67a722468e5a27cef3e2bdecaf
Phil Rukin <philipp@rukin.me>
2018-05-21 13:58:10 +0200

    Increse telegram deadline

--- apis/telegram.py
+++ apis/telegram.py
@@ -17,7 +17,7 @@ def call_method(method, data):
         BASE_URL.format(token=TOKEN, method=method),
         payload=data,
         method=urlfetch.POST,
-        deadline=10,
+        deadline=15,
         headers={'Content-Type': 'application/json'})
   except DeadlineExceededError as e:
     logging.exception(e)
